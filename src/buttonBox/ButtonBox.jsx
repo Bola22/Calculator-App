d698ba8f5cb82d801181a096abdcb6e6635fab38
Bola22 <animashaunbasit22@gmail.com>
2022-10-12 10:58:22 +0100

    First commit

---
+++ src/buttonBox/ButtonBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./buttonBox.css"
+
+function ButtonBox({children}) {
+  return (
+    <div className='buttonBox' >
+      {children}
+    </div>
+  )
+}
+
+export default ButtonBox
---
+++ src/screen/Screen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./screen.css"
+
+
+function Screen({value}) {
+  return (
+    <div className='screen'>
+      {value}
+    </div>
+  )
+}
+
+
+export default Screen
